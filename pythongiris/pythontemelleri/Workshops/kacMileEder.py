f4b3f17b48d9fc026bd61767761ae07c279981f8
Fahrettin İnal <inalfahrettin65@gmail.com>
2026-07-19 22:23:07 +0300

    Workshops completed

---
+++ pythongiris/pythontemelleri/Workshops/kacMileEder.py
@@ -0,0 +1,6 @@
+
+donusumOrani = 0.621371192
+km = int(input("Kaç km?"))
+
+mil = km * donusumOrani
+print(str(km) + "Km = " + str(mil) + " mil eder")
---
+++ pythongiris/pythontemelleri/Workshops/sayilarinyerinidegistir.py
@@ -0,0 +1,10 @@
+x = 10
+y = 20
+
+x,y = y,x
+#temp = x
+#x = y
+#y = temp
+
+print("x = " + str(x))
+print("y = " + str(y))
