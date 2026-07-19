6e50c132b10f886547bec31ee566d0a86a2faa1f
Fahrettin İnal <inalfahrettin65@gmail.com>
2026-07-19 23:27:05 +0300

    pythonVeriYapileri completed

--- pythonVeriYapileri/list.py
+++ pythonVeriYapileri/list.py
@@ -1,3 +1,22 @@
-ogrenci1 = "Engin"
-ogrenci2 = "Derin"
-ogrenci3 = "Salih"
+#ogrenci1 = "Engin"
+#ogrenci2 = "Derin"
+#ogrenci3 = "Salih"
+
+#print(ogrenci1)
+#print(ogrenci2)
+#print(ogrenci3)
+
+ogrenciler = ["Engin", "Derin", "Salih"]
+
+
+ogrenciler.append("Ahmet")
+#ogrenciler[4] ="Veli"
+print(ogrenciler[3])
+ogrenciler.remove("Salih")
+print(ogrenciler)
+
+#list constructor
+sehirler = list(("Ankara", "İstanbul"))
+print(sehirler)
+print(len(sehirler))
+
